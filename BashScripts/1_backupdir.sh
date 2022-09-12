0af8765403a4c731bd5ebd65bd2c5a3eb674ec69
Artjom Lodis <lodis.artem@yandex.ru>
2022-09-12 09:49:23 +0300

    Add file of task 3 and modified file of task 1

--- BashScripts/1_backupdir.sh
+++ BashScripts/1_backupdir.sh
@@ -5,6 +5,7 @@ backup_dir="$2"
 amount_backup="$3"
 date_now=`date +"%H.%M.%S_%d-%m-%Y"`
 
+# создание папки из которой делаем backup
 if [ -d /home/vagrant/"$dir_backup" ]; then
 echo "Directory $dir_backup was created earlier"
 else
@@ -12,14 +13,17 @@ mkdir /home/vagrant/"$dir_backup"
 cp /home/vagrant/Vagrantfile /home/vagrant/"$dir_backup"
 fi
 
+# создание папки для хранения backup
 if [ -d /home/vagrant/"$backup_dir" ]; then
 echo "Directory $backup_dir was created earlier"
 else
 mkdir /home/vagrant/"$backup_dir"
 fi
 
+# архивирование папки
 tar -czf "$dir_backup-$date_now".tar.gz "$dir_backup"
 
+# проверка условия на количество backup в папке и перемещение в папку с backup
 file_count=`find /home/vagrant/"$backup_dir" -type f | wc -l`
 
 if [ "$file_count" -ge "$amount_backup" ]; then

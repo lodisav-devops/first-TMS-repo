0c352f935bffa6f60d8a7cea9c949318a3e8f2df
Artjom Lodis <lodis.artem@yandex.ru>
2022-09-22 10:17:56 +0300

    Modify script 2

--- BashScripts/homework3/2_gener_note_hard_ver.sh
+++ BashScripts/homework3/2_gener_note_hard_ver.sh
@@ -3,7 +3,7 @@
 function createRandomDirFiles {
 
     #генерируем Рандом больше 4
-    a=(4 5 6 7 8 9)
+    a=(5 6 7 8 9)
     b="${#a[@]}"
     c="$((RANDOM%$b))"
     d="${a[$c]}"
@@ -15,7 +15,7 @@ function createRandomDirFiles {
     #выполняем условие рандомного создания папок и файлов с заданной глубиной
     if [ "$2" -ge "1" ]; then
 
-        while [ "$rand_dir" -ge "4" ] && [ "$depth_gen" -ge "1" ]; do
+        while [ "$rand_dir" -ge "1" ] && [ "$depth_gen" -ge "1" ]; do
 
             path="$1/Dir_$depth_gen.$rand_dir"
             mkdir -p "$path"
@@ -24,16 +24,15 @@ function createRandomDirFiles {
 
         done
 
-        while [ "$rand_file" -ge "4" ]; do
+        while [ "$rand_file" -ge "1" ]; do
 
-            #выполняем требование по записи в четные и нечетные файлы соответсвующих записей если принимать что файлов min 4,
-            #то файл 1 он же будет xxx.4.txt и т.д.
+            #проверяем на четность и нечетность через остаток от деления на 2
             if [ $(($rand_file % 2)) -ne 1 ]; then
 
-                echo "odd" > $1/file_$rand_file.txt
-
-            else
                 echo "even" > $1/file_$rand_file.txt
+                
+            else
+                echo "odd" > $1/file_$rand_file.txt
             fi
             
             (( rand_file-- ))

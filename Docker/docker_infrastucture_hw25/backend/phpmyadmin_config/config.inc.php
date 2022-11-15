117b0a4b1d9ced3dd261080d99e875ad4e93be21
Artjom Lodis <lodis.artem@yandex.ru>
2022-11-15 10:09:54 +0300

    Added homework of class 25 (Docker Infrastructure)

---
+++ Docker/docker_infrastucture_hw25/backend/phpmyadmin_config/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+/* Override Servers array */
+$cfg['Servers'] = [
+    1 => [
+        'auth_type' => 'cookie',
+        'host' => 'db',
+        'port' => 3306,
+        'verbose' => 'Verbose name 1',
+    ],
+];
